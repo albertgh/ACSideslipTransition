1bc2d984ad8d0418a4302adfdaee68cc482d877a
Albert Chu <albertzyk@gmail.com>
2013-12-24 15:01:28 +0800

    对齐。。

--- ACSideslipTransition/ACSideslipTransition/ACSideslipMacros.h
+++ ACSideslipTransition/ACSideslipTransition/ACSideslipMacros.h
@@ -23,10 +23,10 @@
 #define DISMISS_DURATION            0.3f            // 解散动画时间
 
 
-#define SWIPE_PERCENT               0.5f           // 前视图滑动距离占屏幕多少百分比后松开完成解散视图
+#define SWIPE_PERCENT               0.5f            // 前视图滑动距离占屏幕多少百分比后松开完成解散视图
 
 
-#define BLACK_MASK_ALPHA_MAX        0.2            // 处于 上层 与 下层 之间的黑色遮罩 最大透明度
+#define BLACK_MASK_ALPHA_MAX        0.2             // 处于 上层 与 下层 之间的黑色遮罩 最大透明度
 #define SHADOW_MASK_ALPHA_MAX       0.6             // 处于 上层 VC 之下的阴影遮罩 最大透明度
 
 
